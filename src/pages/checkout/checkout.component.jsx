51b684da857ca1dd2e63b5b38f9312c2b683c7ec
fanyBaltazar <sara.baltazarak@hotmail.com>
2021-05-11 21:29:47 -0500

    add and remove items

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -25,16 +25,14 @@ const CheckoutPage = ({cartItems, total}) =>(
                 <span>Remove</span>
             </div>
         </div>
-        <div>
-            {
-                cartItems.map(cartItem => 
-                    <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-                )
-            }
-        </div>
+        {
+            cartItems.map(cartItem => 
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+            )
+        }
         <div className='total'>
-                <span>TOTAL: {total}</span>
-            </div>
+            <span>TOTAL: {total}</span>
+        </div>
     </div>
 )
 
