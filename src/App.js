fe497c7a87672ae0f74cde7e088fd50304f09db4
fanyBaltazar <sara.baltazarak@hotmail.com>
2021-05-11 20:48:19 -0500

    add checkout page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignPage from './pages/signpage/sign.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import {auth, createUserProfileDocument} from './firebase/firebase.util'
@@ -43,6 +44,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={HomePage}/>        
           <Route exact path='/shop' component={ShopPage}/>        
+          <Route exact path='/checkout' component={CheckoutPage}/>
           <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/'/> : <SignPage/>} />
         </Switch>
       </div>
