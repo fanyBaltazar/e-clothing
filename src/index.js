6aa72c77717a678c74dd75866f032cb33a35ec62
fanyBaltazar <sara.baltazarak@hotmail.com>
2021-05-16 15:11:32 -0500

    shop sub routing

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import store from  './redux/store';
+import store, { persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
